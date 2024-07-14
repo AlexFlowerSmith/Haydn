4b8a265c5e2a845f7b37670cdeb732de6edf0c3c
Alexander Smith <142947266+AlexanderPFlower@users.noreply.github.com>
2024-07-14 21:10:42 +0000

    Major functionality patch

--- choiceSelector.py
+++ choiceSelector.py
@@ -3,7 +3,7 @@ import sys, time, random
 
 # creates a clear screen function
 def clear():
-    input()
+    input() # empty input intended to slow down speed of program usage / prevent personal overloading
     if name == 'nt':
         _ = system('cls')
     
@@ -27,49 +27,79 @@ def fastprint(str):
         time.sleep(0.010)
 
 
-# menu screen + user selection mechanism
+# pre-menu screen + user selection mechanism
 def selector():
- 
-    input("Press any key to start")
+    """basic pre-menu fun"""
+    choice = input("Press any key to start")
+    if choice == '1':  # Secret option to skip ahead to playGame()
+        playGame()
     clear()
     fastprint("+-+-+-+-+-+-+-+-")
-    slowprint("   Haydn's Very Good Selection Program ")
+    slowprint(" Haydn's Very Good Selection Program ")
     fastprint("+-+-+-+-+-+-+-+-\n")
     fastprint("This program will help you decide what the fuck to do! :)\n")
-    slowprint("ATTENTION\nThis program is a first version demo, so give three options every time!\n")
-    slowprint("Eventually I'll figure out the logic for x amount of choices\n")
-    choices = str(input("How many options do you have?\n> "))
-    optionOne = str(input("First Option:\n> "))
-    optionTwo = str(input("Second Option:\n> "))
-    optionThree = str(input("Third Option:\n> "))
-    secretOption = "Mine Sweeper\n HA HA HA BIG BEAN "
-    tehArray = [optionOne, optionTwo, optionThree, secretOption]
-    tehAnswer = random.choice(tehArray)
+    menuScreen() # main menu screen recalled without animated print
 
-    if tehAnswer == optionOne:
-        tehAnswer = optionOne
-    elif tehAnswer == optionTwo:
-        tehAnswer = optionTwo
-    elif tehAnswer == optionThree:
-        tehAnswer = optionThree
-    elif tehAnswer == secretOption:
-        tehAnswer = secretOption
+
+def playGame():
+    secretOption = "Mine Sweeper\nHA HA HA BIG BEAN"
+
+    choices = input("How many options do you have?\n> ")
+    choices = int(choices)       # typecast choices from str to int
+    choice_list = [secretOption] # initialise list with secretOption already included
+    for i in range(1, choices + 1): # ensure starting index is 1
+        choice = input(f"Name of number {i} choice: ")
+        choice_list.append(choice)
+    slowprint("The anwser is......\n")
+    answer = random.choice(choice_list)
+    #decider(choices, choice_list, answer, secretOption) < old testing function
+    if answer != secretOption:
+        input()
+        print(answer)
+        menuScreen()
     else:
+        input()
         print("ERROR. HA HA BIG BEAN YOU FUCKED IT UP.\n")
-        fastprint("!!!!!!!!!!!!!@@@@@@@@@@@@@!!!!!!!!!!!!!!!!")
+        fastprint("!!!!!!!!!!!!!@@@@@@@@@@@@@!!!!!!!!!!!!!!!!\n")
+        print(f"You MUST choose {secretOption} :)\n")
+        clear()
+        menuScreen()
 
-    fastprint("Thinking.................\n")
-    fastprint("Checking if you are on the FBI most wanted\n")
-    fastprint("Checking the CIA records, too\n")
-    fastprint("Checking Domingo's Friends List\n")
-    fastprint("Deciding if I want a Zambrero burrito or GYG\n")
-    fastprint("Yeah maybe not, I gotta be better\n")
-    fastprint("LOADING ANSWER...........")
-    fastprint("IT'S CUMMING..........")
+
+def menuScreen():
+    """Display menu with print, not fast/slow print, and call menuScreenSelector() for input"""
     clear()
-    print(tehAnswer + " :)\n")
+    print("+-+-+-+-+-+-+-+- Haydn's Very Good Selection Program +-+-+-+-+-+-+-+-\n")
+    fastprint("1. Play  2. Patch Notes  3.  Quit\n")
+    menuScreenSelector()
 
 
+def menuScreenSelector():
+    """ Menu screen input handling function"""
+    menu_choice = input("> ")
+    if menu_choice == '1':
+        playGame()
+    elif menu_choice == '2':
+        patchNotes()
+    else:
+        quit()
 
 
+def patchNotes():
+    """Disply patch notes and option to return to menu"""
+    clear()
+    print("+-+-+-+-+-+-+-+-+-+- Patch Notes from a Dumb Dev +-+-+-+-+-+-+-+-+-+-\n")
+    fastprint("ATTENTION\nWow, 16th September '23 was V1? That's wild.\n")
+    fastprint("Now I'm making a V2 at friggin 0530 in the morning lol\n")
+    input("\nENTER to return to menu")
+    menuScreen()
+
+''' Old testing function
+def decider(choices, choice_list, answer, secretOption):
+    # Testing outputs, choices + length of list + answer
+    print(f"The number of choices: {choices}")
+    print(len(choice_list))
+    print(answer)
+''' 
+    
 selector()
